80cac2b3678e1e63c30d4d96ccab2ab15b344666
John Lam <jlam@iunknown.com>
2016-01-24 17:50:57 -0800

    cleaned up comments and added Option checking in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,35 @@
-use std::ffi::CStr;
-use std::os::raw::c_char;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
-use std::error::Error;
+extern crate libc;
+
 use std::ptr;
 use std::slice;
-
-extern crate libc;
+use std::error::Error;
+use std::ffi::CStr;
+use std::fs::File;
+use std::io::prelude::*;
+use std::os::raw::c_char;
+use std::path::Path;
 
 use libc::{uint32_t, size_t, c_double};
 
+// Simplest possible call - no parameters, no return value
 #[no_mangle]
 pub extern fn hello() {
     println!("Hello, from Rust!");
 }
 
+// Function call with primitive value (all primitive values should be equivalent here) with
+// primitive value return type
 #[no_mangle]
 pub extern fn square(x: i32) -> i32 {
     x * x
 }
 
+// Helper function to convert a const char* to a Rust String
 fn cstr_to_string(name: *const c_char) -> String {
     return unsafe { CStr::from_ptr(name).to_string_lossy().into_owned() };
 }
 
+// Function call that takes an ANSI const char* as an input parameter
 #[no_mangle]
 pub extern fn say_hello(name: *const c_char) {
     println!("Hello, {}", cstr_to_string(name));
@@ -40,14 +45,19 @@ pub extern fn parse(cstr_path: *const c_char) {
         Ok(file) => file,
     };
     let mut s = String::new();
-    file.read_to_string(&mut s);
-    println!("The file is {}", s);
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("could not read {} because {}", path.display(), 
+                                                           Error::description(&why)),
+        Ok(_) => println!("The length of the file is {}", s.len()), 
+    };
 }
 
 #[no_mangle]
-pub extern fn call_me_back(f_ptr: extern fn(x: i32)) {
-//    assert!(!f_ptr.is_null()); how test for null-ness here?
-    f_ptr(42);
+pub extern fn call_me_back(f_ptr: Option<extern fn(x: i32)>) {
+    match f_ptr {
+        Some(f) => f(42),
+        None => panic!("null function pointer"),
+    };
 }
 
 // This is the simple case where we are passing arrays as in-parameters to a Rust function
@@ -84,52 +94,50 @@ pub extern fn sum_float_array(array_ptr: *const c_double, len: size_t) -> c_doub
 #[no_mangle]
 pub extern fn mutate_numpy_array(array_ptr: *mut c_double, len: size_t) {
 
-    // Note that the numbers reference must be mut as well, and note the &mut reference!
     let mut numbers: &mut [c_double] = unsafe {
         assert!(!array_ptr.is_null());
-
-        // Note the difference is calling ::from_raw_parts_mut()
         slice::from_raw_parts_mut(array_ptr, len as usize)
     };
 
     for i in 0..len {
         numbers[i] = 2.0;
-        println!("number {}", numbers[i]);
     };
 }
 
-#[no_mangle] 
-pub extern fn get_double_from_python(f_ptr: extern fn(number: *mut c_double)) -> f64 {
-//    assert!(!f_ptr.is_null()); how test for nullness here?
+// Note that we are explicitly testing for nullness here in this case and panicing if we 
+// encounter a null function pointer. This shows off using the Option type to encapsulate
+// values that may or may not be present.
 
-    let mut number: f64 = 0.0;
-    f_ptr(&mut number);
-    println!("this is what I got from python: {}", number);
-    number
+#[no_mangle] 
+pub extern fn get_double_from_python(f_ptr: Option<extern fn(number: *mut c_double)>) -> f64 {
+    match f_ptr {
+        Some(f) => {
+            let mut number: f64 = 0.0;
+            f(&mut number);
+            return number
+        },
+        None => panic!("null pointer passed in"),
+    };
 }
 
 #[no_mangle]
-pub extern fn fill_array(f_ptr: extern fn(x: i32, array_ptr: *mut *mut c_double)) {
+pub extern fn fill_array(f_ptr: Option<extern fn(x: i32, array_ptr: *mut *mut c_double)>) {
+    match f_ptr {
+        Some(f) => {
+            // Ask Python to allocate an array with 4 elements
+            let len: usize = 4;
+            let mut array_ptr: *mut c_double = ptr::null_mut();
+            f(4, &mut array_ptr);
 
-    println!("in fill_array");
+            let mut numbers: &mut [c_double] = unsafe {
+                assert!(!(array_ptr).is_null());
+                slice::from_raw_parts_mut(array_ptr, len)
+            };
 
-    // Ask Python to allocate an array with 4 elements
-    let len: usize = 4;
-    let mut array_ptr: *mut c_double = ptr::null_mut();
-    f_ptr(4, &mut array_ptr);
-
-    println!("after calling to python");
-
-    let mut numbers: &mut [c_double] = unsafe {
-        assert!(!(array_ptr).is_null());
-
-        slice::from_raw_parts_mut(array_ptr, len)
+            for i in 0..len {
+                numbers[i] = 42.0;
+            }
+        },
+        None => panic!("null function pointer passed in"),
     };
-
-    println!("before iteration");
-    for i in 0..len {
-        println!("before number = {}", numbers[i]);
-        numbers[i] = 42.0;
-        println!("number = {}", numbers[i]);
-    }
 }
--- test.py
+++ test.py
@@ -1,96 +1,93 @@
-from ctypes import cdll
-from ctypes import CFUNCTYPE, c_int, c_uint32, c_size_t, POINTER, c_long, c_double, c_void_p, py_object
-import numpy as N
+from ctypes import cdll, cast
+from ctypes import CFUNCTYPE, POINTER
+from ctypes import c_int, c_uint32, c_size_t, c_long, c_double, c_void_p, py_object, c_char
+import numpy as numpy
 
-lib = cdll.LoadLibrary("c:/users/jflam/src/rust/parser/target/debug/parser.dll")
+lib = cdll.LoadLibrary("target/debug/parser.dll")
 
 # Pass nothing
+print("Calling Rust function without any parameters or results")
 lib.hello()
 
 # Pass 32 bit int
+print("\nCalling Rust function with a 32 bit integer parameter")
 print("4 squared = {}".format(lib.square(4)))
 
 # Pass python string - is there a way in rust to deal with strings as Unicode
 # vs. ANSI? This would be better than forcing encode on Python side
+print("\nCalling Rust function with an ANSI string")
 lib.say_hello("John".encode("ascii"))
 
-# Do File I/O in rust
-PATH = "c:/users/jflam/src/rust/parser/test.py"
-print("Dumping {} to console using rust lib:".format(PATH))
+# Do File I/O in rust and print the size of the read bytes to the console
+print("\nCalling Rust function with a path to do some file i/o")
+PATH = "test.py"
 lib.parse(PATH.encode("ascii"))
 
-# Trivial callback from rust - note that the first parameter to CFUNCTYPE is the return type
+# Call rust function passing in a Python list converted into a raw byte array
+lib.sum_array.restype = c_uint32
+lib.sum_array.argtypes = (POINTER(c_uint32), c_size_t)
+
+# Helper function that copies the data from the List into a ray byte array
+def sum_array(numbers):
+    # Note that this does a copy of the original data
+    buf_type = c_uint32 * len(numbers)  # compute size
+    buf = buf_type(*numbers) # copy list into the buffer
+    return lib.sum_array(buf, len(numbers))
+
+print("\nCalling Rust function with a Python List converted into a raw array")
+print("sum of [1,2,3,4] = {}".format(sum_array([1,2,3,4])))
+
+# Call Rust function passing in a NumPy array as an in-parameter
+lib.sum_float_array.restype = c_double
+lib.sum_float_array.argtypes = (POINTER(c_double), c_size_t)
+
+# Now let's call it with a Numpy array, which are typically floats
+print("\nCalling Rust function with NumPy array as an in-parameter")
+sum = lib.sum_float_array(numpy.random.rand(1000).ctypes.data_as(POINTER(c_double)), 1000)
+print("Sum of random 1000 numbers in numpy array: {}".format(sum))
+
+# Call Rust function passing in a NumPy array as an in/out parameter
+lib.mutate_numpy_array.restype = None
+lib.mutate_numpy_array.argtypes = (POINTER(c_double), c_size_t)
+
+# Zero'd array will be changed to all 2s 
+numpy_array = numpy.zeros(4)
+
+print("\nCalling Rust function with a mutable NumPy array, and changing the data")
+lib.mutate_numpy_array(numpy_array.ctypes.data_as(POINTER(c_double)), len(numpy_array))
+print("mutated array: {}".format(numpy_array))
+
+# Trivial callback from Rust, where Rust passes an int to Python 
 CALLBACK = CFUNCTYPE(c_int, c_int)
 
 lib.call_me_back.restype = None
 lib.call_me_back.argtypes = [CALLBACK]
 
 def callback(x):
-    print("Received this 32 bit int from rust: {}".format(x))
+    print("  Python: Received this 32 bit int from rust: {}".format(x))
     return x
 
-print("calling rust ...")
+print("\nCalling Rust function with a Python callback function that gets invoked with an integer")
 lib.call_me_back(CALLBACK(callback))
-print("done!");
 
-# Passing native Python lists across the FFI only works for in-parameters only
-# More complex call into a rust function passing an array rust function compute the sum
-
-lib.sum_array.restype = c_uint32
-lib.sum_array.argtypes = (POINTER(c_uint32), c_size_t)
-
-def sum_array(numbers):
-    # Note that this does a copy of the original data
-    buf_type = c_uint32 * len(numbers)  # compute size
-    buf = buf_type(*numbers)
-    return lib.sum_array(buf, len(numbers))
-
-print("sum of [1,2,3,4] = {}".format(sum_array([1,2,3,4])))
-
-lib.sum_float_array.restype = c_double
-lib.sum_float_array.argtypes = (POINTER(c_double), c_size_t)
-
-def sum_float_array(numbers):
-    # Note that this does a copy of the original data
-    buf_type = c_double * len(numbers)
-    buf = buf_type(*numbers)
-    return lib.sum_float_array(buf, len(numbers))
-
-# Now let's call it with a Numpy array, which are typically floats
-print("sum of random 1000 numbers in numpy array: {}",
-        format(sum_float_array(N.random.rand(1000))))
-
-# Now let's see if we can pass a mutable array to rust and have it change on the
-# other side
-
-lib.mutate_numpy_array.restype = None
-lib.mutate_numpy_array.argtypes = (POINTER(c_double), c_size_t)
-
-numpy_array = N.zeros(4)
-numpy_array[0] = 1
-numpy_array[1] = 2
-numpy_array[2] = 3
-numpy_array[3] = 4
-
-print("original array: {}".format(numpy_array))
-lib.mutate_numpy_array(numpy_array.ctypes.data_as(POINTER(c_double)), len(numpy_array))
-print("mutated array: {}".format(numpy_array))
-
-# Call a function which calls a callback in Python that explicitly returns a
+# Call a Rust function which calls a callback in Python that returns a
 # double as an out parameter. Note the pointer dereference semantics in this
 # type.
-
 def callback_double(p_double):
     p_double[0] = 42
-    print("called from rust and trying to return 42")
+    print("  Python: called from Rust and trying to return 42")
 
 CALLBACK_DOUBLE = CFUNCTYPE(None, POINTER(c_double))
 
 lib.get_double_from_python.restype = c_double
 lib.get_double_from_python.argtypes = [CALLBACK_DOUBLE]
 
+print("\nCalling Rust function with a Python callback function that returns a double as an out parameter")
 result = lib.get_double_from_python(CALLBACK_DOUBLE(callback_double))
-print("got {} back from python via rust!".format(result))
+print("Python: got {} back from the Rust function".format(result))
+
+#print("\nCalling Rust function with a null callback function pointer")
+#result = lib.get_double_from_python(cast(None, CALLBACK_DOUBLE))
 
 # More sophisticated callback where I dynamically allocate a numpy array in the
 # callback function that Rust will fill in for me
@@ -107,9 +104,9 @@ g_array = None
 
 def allocate(length, ppResult):
     global g_array
-    g_array = N.zeros(length)
+    g_array = numpy.zeros(length)
     ppResult[0] = g_array.ctypes.data_as(POINTER(c_double))
-    print("here in allocate")
+    print("  Python: allocating a NumPy array of length {}".format(length))
 
 # fill_array will call back into Python to the allocate() function defined 
 # above to allocate a NumPy array of sufficient size and fill that array with 
@@ -123,6 +120,10 @@ ALLOCATOR = CFUNCTYPE(None, c_int, POINTER(POINTER(c_double)))
 lib.fill_array.restype = None
 lib.fill_array.argtypes = [ALLOCATOR]
 
+print("\nCalling Rust function that will fill a Python-allocated NumPy array with values")
 lib.fill_array(ALLOCATOR(allocate)) 
 print("array is: {}".format(g_array))
 print("and the total computed using numpy is {}".format(g_array.sum()))
+
+# print("\nCalling Rust function with a null callback function pointer")
+# lib.fill_array(cast(None, ALLOCATOR))
