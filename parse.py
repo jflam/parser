e4caf0689d1f1316c68016df933259d4fee6f5c4
John Lam <jlam@iunknown.com>
2016-01-30 14:41:12 -0800

    Changed parse.py to be a benchmark test for the performance of the CRC routine

--- parse.py
+++ parse.py
@@ -1,7 +1,34 @@
+import os
+import time
 from cffi import FFI
 ffi = FFI()
 
-lib = ffi.dlopen("target/debug/parser.dll")
+lib = ffi.dlopen("target/release/parser.dll")
 ffi.cdef("void compute_file_crc(const char*);")
 
-lib.compute_file_crc("test/2015-12-23-14-32-19.fit".encode("ascii"))
+class Timer(object):
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.time()
+        self.secs = self.end - self.start
+        self.msecs = self.secs * 1000  # millisecs
+        if self.verbose:
+            print('elapsed time: %f ms' % self.msecs)
+
+FILENAME = "test/2015-12-23-14-32-19.fit"
+
+file_length = os.stat(FILENAME).st_size
+
+with Timer() as t:
+    lib.compute_file_crc(FILENAME.encode("ascii"))
+
+file_length_mb = file_length / 1000000
+megabytes_per_second = file_length_mb / t.secs
+print("Elapsed time: {}ms".format(t.msecs))
+print("Velocity: {}MB/s".format(megabytes_per_second))
